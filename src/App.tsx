d7b81a5b0fccf3dee2eb78e8d55f7a4f2a654fad
andrewc2020 <andrewc2020@gmail.com>
2022-01-02 18:01:09 +0000

    added typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+const logo = require("./logo.svg") as string;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hello world
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
